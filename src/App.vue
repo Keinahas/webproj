add295b7a4ec6f2e1acdbb37bc83717cbf717ac9
unknown <woochan909@mju.ac.kr>
2020-11-19 14:49:11 +0900

    Design example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
 	<v-app>
-		<v-app-bar color="primary" dark app>
+		<v-app-bar dark app src="https://i.ibb.co/BHDhCqk/background.jpg">
+			<template v-slot:img="{ props }">
+				<v-img v-bind="props" gradient="to top right, rgba(0,0,0,.7), rgba(25,32,72,.7)"></v-img>
+			</template>
 			<v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-			<v-toolbar-title>Title</v-toolbar-title>
+			<v-toolbar-title class="text-uppercase white--text">
+				<span><h3>Database Design</h3></span>
+			</v-toolbar-title>
 			<v-spacer></v-spacer>
-			<v-btn v-if="!userId" color="primary" class="elevation-0" to="register"
+			<v-btn v-if="!userId" class="ma-2" outlined color="white" to="register"
 				><v-icon left>mdi-login</v-icon>Register</v-btn
 			>
 			<v-btn v-else color="primary" class="elevation-0" to="mypage"
 				><v-icon left>mdi-account-circle</v-icon>MyPage</v-btn
 			>
-			<v-btn v-if="!userId" color="primary" class="elevation-0" to="login"
+			<v-btn v-if="!userId" class="ma-2" outlined color="white" to="login"
 				><v-icon left>mdi-login</v-icon>Login</v-btn
 			>
 			<v-btn v-else color="primary" class="elevation-0" @click="$store.commit('logout')"
@@ -29,7 +34,11 @@
 <script>
 export default {
 	name: "App",
-
+	methods: {
+		goMain() {
+			this.$router.push("/");
+		},
+	},
 	components: {},
 	computed: {
 		userId() {

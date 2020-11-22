3e495337057d0e467f271f316823c65219c2ecf0
IllIlIlIllIl <12__3__45_67@naver.com>
2020-11-23 02:00:33 +0900

    사원관리/ 프로젝트관리 추가, 프로젝트 card형 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,53 +31,6 @@
 					<h4>2020.10.10</h4>
 					<h4>Database design News2</h4>
 				</v-col>
-				<v-col cols="12"></v-col>
-				<v-col cols="12"></v-col>
-				<v-progress-linear
-					background-color="primary"
-					color="#030712"
-					value="30"
-				></v-progress-linear>
-				<v-col cols="12"></v-col>
-				<v-col cols="12"></v-col>
-				<v-col id="News1" cols="3">
-					<h3>소개</h3>
-					<br />
-					<h4>Database Design 소개</h4>
-				</v-col>
-				<v-col id="News1" cols="3">
-					<h3>미디어</h3>
-					<br />
-					<h4>Database Design 보도 자료</h4>
-					<h4>미디어 행사</h4>
-				</v-col>
-				<v-col id="News1" cols="3">
-					<h3>채용</h3>
-					<br />
-					<h4>Database Design 채용 소개</h4>
-					<h4>지원하기</h4>
-				</v-col>
-				<v-col id="News1" cols="3">
-					<h3>투자정보</h3>
-					<br />
-					<h4>Database Design 추가 정보</h4>
-					<h4>Database Design 재무 정보</h4>
-					<h4>IR 자료</h4>
-				</v-col>
-				<v-col cols="12"></v-col>
-				<v-col cols="12"></v-col>
-				<v-col cols="2"></v-col>
-				<v-col cols="3"
-					><p class="font-weight-black">
-						DATABASE DESIGN
-					</p></v-col
-				>
-				<v-col cols="1"></v-col>
-				<v-col cols="6"
-					><p class="text--primary">
-						copyright @Database Design Crop. All Rights Reserved
-					</p></v-col
-				>
 			</v-row>
 		</v-container>
 		<v-container v-else color="secondary" class="fill-height">
@@ -98,33 +51,67 @@
 				<v-col cols="12">
 					<h2>참여중인 프로젝트</h2>
 				</v-col>
-				<v-col cols="12">
-					<h1>프로젝트01</h1>
-					<h3>2020.10.23</h3>
-					<br />
-					<h3>PM : 김피엠</h3>
-					<br />
+
+				<v-col cols="5">
+					<v-col cols="2"></v-col>
+					<v-col cols="10">
+						<template>
+							<v-card outlined shaped>
+								<v-list-item three-line>
+									<v-list-item-content>
+										<div class="overline pt-2">2020.10.23</div>
+										<v-list-item-title class="headline mb-1">프로젝트01</v-list-item-title>
+										<v-list-item-subtitle>PM : 김피엠</v-list-item-subtitle>
+									</v-list-item-content>
+
+									<v-spacer></v-spacer>
+								</v-list-item>
+							</v-card>
+						</template>
+					</v-col>
 				</v-col>
+				<v-col cols="12"></v-col>
 				<v-col cols="12"></v-col>
 				<v-col cols="12"></v-col>
 				<v-col cols="12">
 					<h2>참여했던 프로젝트</h2>
 				</v-col>
 				<v-col cols="5">
-					<h1>프로젝트02</h1>
-					<h3>2020.00.00</h3>
-					<br />
-					<h3>PM : 이피엠</h3>
-					<br />
+					<v-col cols="2"></v-col>
+					<v-col cols="10">
+						<v-card outlined shaped>
+							<v-list-item three-line>
+								<v-list-item-content>
+									<div class="overline pt-2">2020.00.00</div>
+									<v-list-item-title class="headline mb-1">프로젝트02</v-list-item-title>
+									<v-list-item-subtitle>PM : 이피엠</v-list-item-subtitle>
+								</v-list-item-content>
+
+								<v-spacer></v-spacer>
+							</v-list-item>
+						</v-card>
+					</v-col>
 				</v-col>
+				<v-col cols="1"></v-col>
 				<v-col cols="5">
-					<h1>프로젝트03</h1>
-					<h3>2020.00.00</h3>
-					<br />
-					<h3>PM : 박피엠</h3>
-					<br />
+					<v-col cols="2"></v-col>
+					<v-col cols="10">
+						<v-card outlined shaped>
+							<v-list-item three-line>
+								<v-list-item-content>
+									<div class="overline pt-2">2020.00.00</div>
+									<v-list-item-title class="headline mb-1">프로젝트03</v-list-item-title>
+									<v-list-item-subtitle>PM : 박피엠</v-list-item-subtitle>
+								</v-list-item-content>
+
+								<v-spacer></v-spacer>
+							</v-list-item>
+						</v-card>
+					</v-col>
 				</v-col>
-				<v-col cols="2">
+				<v-col cols="1">
+					<br />
+					<br />
 					<br />
 					<br />
 					<br />
@@ -135,53 +122,6 @@
 					>
 				</v-col>
 				<v-col cols="12"></v-col>
-				<v-col cols="12"></v-col>
-
-				<v-progress-linear
-					background-color="primary"
-					color="#030712"
-					value="30"
-				></v-progress-linear>
-				<v-col cols="12"></v-col>
-				<v-col cols="12"></v-col>
-				<v-col id="News1" cols="3">
-					<h3>소개</h3>
-					<br />
-					<h4>Database Design 소개</h4>
-				</v-col>
-				<v-col id="News1" cols="3">
-					<h3>미디어</h3>
-					<br />
-					<h4>Database Design 보도 자료</h4>
-					<h4>미디어 행사</h4>
-				</v-col>
-				<v-col id="News1" cols="3">
-					<h3>채용</h3>
-					<br />
-					<h4>Database Design 채용 소개</h4>
-					<h4>지원하기</h4>
-				</v-col>
-				<v-col id="News1" cols="3">
-					<h3>투자정보</h3>
-					<br />
-					<h4>Database Design 추가 정보</h4>
-					<h4>Database Design 재무 정보</h4>
-					<h4>IR 자료</h4>
-				</v-col>
-				<v-col cols="12"></v-col>
-				<v-col cols="12"></v-col>
-				<v-col cols="2"></v-col>
-				<v-col cols="3"
-					><p class="font-weight-black">
-						DATABASE DESIGN
-					</p></v-col
-				>
-				<v-col cols="1"></v-col>
-				<v-col cols="6"
-					><p class="text--primary">
-						copyright @Database Design Crop. All Rights Reserved
-					</p></v-col
-				>
 			</v-row>
 		</v-container>
 	</v-container>
